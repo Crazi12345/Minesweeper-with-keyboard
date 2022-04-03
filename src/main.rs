1892a365648be66fe7b55bfc219e5bfe9961f8b5
Patrick <Patrickbjerre@outlook.dk>
2022-04-03 17:25:03 +0200

    Made bomb generation and lose condition

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use std::os::raw::c_char;
 use std::{thread::sleep, time::Duration};
 use std::io::Cursor;
 
+use colored::Colorize;
+use rand::Rng;
+
 const discovered: char = '□';
 const cursor: char = '⛶';
 const undiscovered: char = '■';
@@ -12,14 +15,24 @@ const flag: char = '◩';
 static mut cursor_x: usize = 0;
 static mut cursor_y: usize = 0;
 static mut course: [[char; 30]; 16] = [[undiscovered;30];16];
+static mut mines: [[i8;30];16]= [[-1;30];16];
 
 
 fn PrettyPrint(courses: [[char; 30]; 16], cx: usize, cy: usize) {
+    //you can use a control character
+
+
+        print!("{}[2J", 27 as char);
+
+
     for x in 0..16 {
         for y in 0..30 {
             if cx == x && cy == y {
-                print!("  {}", cursor);
-            } else {
+                print!("  {}", format!("{}",cursor).yellow());
+            } else if courses[x][y] == flag{
+                print!("  {}", format!("{}",flag).red())
+            }
+            else{
                 print!("  {}", courses[x][y]);
             }
         }
@@ -55,6 +68,7 @@ unsafe fn subY() {
     }
 }
 
+
 unsafe fn setFlag() {
     if course[cursor_x][cursor_y] == undiscovered{
         course[cursor_x][cursor_y]= flag;}
@@ -64,20 +78,47 @@ unsafe fn setFlag() {
 
 
 }
-/*
-  fn generateCourse() -> [[char; 30]; 16] {
-    let mut courseTemp: [[char; 30]; 16]=[[undiscovered;30];16] ;
+
+unsafe fn lose(){
+
     for x in 0..16 {
         for y in 0..30 {
-            courseTemp[x][y] = undiscovered;
+            if mines[x][y]==1 {
+                print!("  {}", format!("{}",'*').red());
+            } else {
+                print!("  {}", format!("{}",discovered).black())
+            }
 
-            print!("  {}", courseTemp[x][y]);
         }
         println!();
     }
-    courseTemp[5][0] = '5';
-    return courseTemp;
-}*/
+    std::process::exit(0);
+}
+
+unsafe fn openField() {
+    if course[cursor_x][cursor_y] == undiscovered && mines[cursor_x][cursor_y]==1{
+        course[cursor_x][cursor_y] = '*';
+        lose();
+    } else if course[cursor_x][cursor_y] == undiscovered && mines[cursor_x][cursor_y]==-1{
+        course[cursor_x][cursor_y] = discovered;
+    }
+    PrettyPrint(course,cursor_x,cursor_y);
+}
+
+  unsafe fn generateMines() {
+      let mut mineNum: i16 = 100;
+      let mut rng = rand::thread_rng();
+      while mineNum != 0 {
+          let x: usize = rng.gen::<usize>() % 16;
+          let y: usize = rng.gen::<usize>() % 30;
+          if mines[x][y] == -1 {
+              mines[x][y] = 1;
+              mineNum = mineNum - 1;
+          }
+      }
+  }
+
+
 
 fn main() {
 
@@ -88,25 +129,28 @@ fn main() {
 
 
     unsafe {
+        generateMines();
         WKey.bind(move || {
             subX();
             PrettyPrint(course, cursor_x, cursor_y)
         });
         SKey.bind(move || {
             addX();
-            SafeCallPrettyPrint(course, cursor_x, cursor_y)
+            PrettyPrint(course, cursor_x, cursor_y)
         });
         DKey.bind(move || {
             addY();
-            SafeCallPrettyPrint(course, cursor_x, cursor_y)
+            PrettyPrint(course, cursor_x, cursor_y)
         });
         AKey.bind(move || {
             subY();
-            SafeCallPrettyPrint(course, cursor_x, cursor_y)
+            PrettyPrint(course, cursor_x, cursor_y)
         });
         EnterKey.bind(move|| { setFlag(); PrettyPrint(course,cursor_x,cursor_y) } );
+        SpaceKey.bind(move || {openField();});
         QKey.bind(|| MouseCursor::move_rel(10, 10));
     }
-    handle_input_events();
+    loop{
+    handle_input_events();}
     println!("HERE");
 }
